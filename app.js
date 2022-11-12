4798987b65eb628c8c29264437f6489a5574e2ce
Izabella Chebakova <chebakovaizabella@gmail.com>
2022-11-12 19:33:21 +0300

    Удаление body-parser, использование вместо него express.json()

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const user = require('./routes/users');
 const card = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
