1bf42e8f4bed1b7ef35a330a4644ea545b2a7a13
Izabella Chebakova <chebakovaizabella@gmail.com>
2022-11-22 18:39:54 +0300

    Добал=вление централизованной обработки ошибки при ненайденной странице

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const { StatusCodes } = require('http-status-codes');
+// const { StatusCodes } = require('http-status-codes');
 const auth = require('../middlewares/auth');
 const user = require('./users');
 const card = require('./cards');
 const { login, createUser } = require('../controllers/users');
-const { PAGE_NOT_FOUND_MESSAGE } = require('../constants');
+const PageNotFoundError = require('../errors/pageNotFoundError');
+// const { PAGE_NOT_FOUND_MESSAGE } = require('../constants');
 
 router.post('/signin', login);
 router.post('/signup', createUser);
@@ -14,8 +15,8 @@ router.use(auth);
 router.use('/users', user);
 router.use('/cards', card);
 
-router.use('/', (req, res) => {
-  res.status(StatusCodes.NOT_FOUND).send({ message: PAGE_NOT_FOUND_MESSAGE });
+router.use('/', (req, res, next) => {
+  next(new PageNotFoundError());
 });
 
 module.exports = router;
