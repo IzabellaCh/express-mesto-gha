dde75089af532955f56de4ed131cb46304524884
Izabella Chebakova <chebakovaizabella@gmail.com>
2022-11-24 17:36:14 +0300

    Установка и подключение helmet и express-rate-limit

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
@@ -8,6 +10,16 @@ const handleError = require('./middlewares/handleError');
 
 const app = express();
 
+// ограничение количества запросов
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standartHeaders: true,
+  legacyHeaders: true,
+});
+app.use(limiter);
+// настройка http-заголовков
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
